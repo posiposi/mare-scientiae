6dddbc52cf9e432b11eb567df285142227b22760
Daichi Sugiyama <caad10.shimano105@gmail.com>
2026-04-22 10:03:14 +0900

    remove unnecessary comment

--- internal/domain/repository/book_repository.go
+++ internal/domain/repository/book_repository.go
@@ -6,8 +6,6 @@ import (
 	"helloworld/internal/domain/model"
 )
 
-// BookQueryRepositorier defines read-side persistence operations for
-// Book aggregates (CQRS query side).
 type BookQueryRepositorier interface {
 	FindAll(ctx context.Context) ([]*model.Book, error)
 }
